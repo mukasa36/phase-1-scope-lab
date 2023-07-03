0974d191996b4f0686fc120aaaf939912ff17c31
emmanuel <emmanuel.mukasa@moringaschool.com>
2023-07-04 02:04:35 +0300

    Lab done

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution in this file!
+var customerName = "bob";
+const leastFavoriteCustomer="nada";
+
+function upperCaseCustomerName() {
+
+    return customerName.toUpperCase();
+}
+
+var bestCustomer;
+
+function setBestCustomer(){
+
+    bestCustomer ='not bob';
+
+}
+
+function overwriteBestCustomer(){
+    bestCustomer ='maybe bob'
+}
+
+function changeLeastFavoriteCustomer(){
+     leastFavoriteCustomer = "me";
+    return leastFavoriteCustomer;
+
+}
+console.log(changeLeastFavoriteCustomer());
+
+
+//upperCaseCustomerName(customerName);
+//setBestCustomer()
+//console.log(customerName)file!
